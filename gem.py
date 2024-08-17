b03353f803572cdaa81a05cf1818764347d99b06
mitbingoo <nguyentuduyanh@gmail.com>
2024-08-17 17:48:22 +0700

    a

--- gem.py
+++ gem.py
@@ -89,7 +89,7 @@ def create_gem_folders(groups, tools_path):
                 github_url = "https://raw.githubusercontent.com/mitbingoo/robloxtools/main/script/gem.txt"
                 response = requests.get(github_url)
                 response = response.content.decode('utf-8').replace('\r\n', '\n')  # Decode and replace newline characters
-                response = response.text.replace("{user}", user)
+                response = response.replace("{user}", user)
                 with open(user_file, 'w') as uf:
                     uf.write(response)
                 print(f"Created {user_file} for Group {group_number}")
